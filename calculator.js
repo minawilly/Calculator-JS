ff822c65464d55afeb5a3c522c191b66e8e02e37
minawilly <aminahwilliamsade@gmail.com>
2022-06-22 22:41:51 +0100

    Initial Commit

---
+++ calculator.js
@@ -0,0 +1,38 @@
+let result;
+
+const operations = prompt("Calculator: What operation will you like to perform? (+, -, *, /, %)");
+const num1 = parseFloat(prompt("Enter First Number"));
+const num2 = parseFloat(prompt("Enter Second Number"));
+
+switch (operations) {
+    case "+":
+        result = num1 + num2;
+        console.log('${num1} + ${num2} = ${result}');
+        break;
+
+    case "-":
+        result = num1 - num2;
+        console.log('${num1} - ${num2} = ${result}');
+        break;
+
+    case "*":
+        result = num1 * num2;
+        console.log('${num1} * ${num2} = ${result}');
+        break;
+
+    case "/":
+        result = num1 / num2;
+        console.log('${num1} / ${num2} = ${result}');
+        break;
+
+    case "%":
+        result = num1 % num2;
+        console.log('${num1} % ${num2} = ${result}');
+        break;
+
+    default:
+        console.log("Invalid operation");
+        break;
+}
+
+alert(result);
